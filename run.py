5e5b7c913418d94be18ee879cca67619fe5ef760
Brogan Carpenter <Brogancarpenter@hotmail.co.uk>
2024-07-15 12:22:39 +0000

    Add in messaging for the user to select their question

--- run.py
+++ run.py
@@ -23,8 +23,22 @@ records = worksheet.get_all_records()
 # Convert the records to a DataFrame
 df = pd.DataFrame(records)
 
-# Display the first few rows of the DataFrame
-print(df.head())
 
-# Perform data analysis (e.g., descriptive statistics)
-print(df.describe())
+def get_survey_data():
+    """
+    Get survey data from the google sheet / taylorswift_erastour.
+    """
+    print("Find out more from our Eras Tour Survey\n")
+    print("1. How many females compared to males were there?")
+    print("2. What is the average age of the fans?")
+    print("3. What country had the most fans attending?")
+    print("4. What was the favourite album of the fans?")
+    print("5. What was the favourite song from the fans?")
+    print("6. What was the lowest inputted favourite song and album?")
+    print("7. What was the average age of 'Reputation' fans?")
+    print("8. What was the majority gender for fans of 'Lover'? \n")
+
+    data_str = input("Enter the question number here: ")
+    print(f"The data provided is {data_str}")
+
+get_survey_data()
